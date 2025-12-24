108dcbdafda44c6df7bd9bef60caf559752641a9
parteekdhiman <pkdhiman414@gmail.com>
2025-12-24 15:22:46 +0530

    first commit

--- hello.js
+++ hello.js
@@ -1,15 +1,7 @@
-// Example API route that returns a simple JSON response
-// const express = require("express");
-import express from ("express")
-const router = express.Router();
-
-// Simple hello endpoint
-router.get("/", (req, res) => {
+// Simple serverless hello function - CommonJS
+module.exports = (req, res) => {
   res.json({
     message: "Hello from Vercel Serverless Function!",
     timestamp: new Date().toISOString(),
   });
-});
-
-// Export the router
-module.exports = router;
+};
